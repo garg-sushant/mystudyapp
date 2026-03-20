24b35c43e9ff9fa1eecd5efd495a8c7b3b3736b3
Sushant <sgarg9031@gmail.com>
2026-03-20 14:07:45 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
 import AppNavbar from './components/Navbar'
@@ -12,58 +12,39 @@ import Login from './components/Login'
 import ProtectedRoute from './components/ProtectedRoute'
 import './App.css'
 
+function Layout() {
+  const location = useLocation()
+
+  return (
+    <div className="App">
+      
+      {/* ✅ Hide navbar on login */}
+      {location.pathname !== '/login' && <AppNavbar />}
+
+      <main>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="/schedule" element={<ProtectedRoute><Schedule /></ProtectedRoute>} />
+          <Route path="/goals" element={<ProtectedRoute><Goals /></ProtectedRoute>} />
+          <Route path="/analytics" element={<ProtectedRoute><Analytics /></ProtectedRoute>} />
+
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
+
+      {/* Optional: also hide footer on login */}
+      {location.pathname !== '/login' && <Footer />}
+    </div>
+  )
+}
+
 function App() {
   return (
     <Provider store={store}>
       <Router>
-        <div className="App">
-          <AppNavbar />
-          <main>
-            <Routes>
-              <Route path="/login" element={<Login />} />
-
-              <Route
-                path="/"
-                element={
-                  <ProtectedRoute>
-                    <Dashboard />
-                  </ProtectedRoute>
-                }
-              />
-
-              <Route
-                path="/schedule"
-                element={
-                  <ProtectedRoute>
-                    <Schedule />
-                  </ProtectedRoute>
-                }
-              />
-
-              <Route
-                path="/goals"
-                element={
-                  <ProtectedRoute>
-                    <Goals />
-                  </ProtectedRoute>
-                }
-              />
-
-              <Route
-                path="/analytics"
-                element={
-                  <ProtectedRoute>
-                    <Analytics />
-                  </ProtectedRoute>
-                }
-              />
-
-              {/* ✅ fallback route */}
-              <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
+        <Layout />
       </Router>
     </Provider>
   )
